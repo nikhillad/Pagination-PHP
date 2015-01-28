c07160be20d8c1ccfa2c9dd92a4a93e916052977
Nikhil Lad <nikhil.lad@disney.com>
2015-01-29 01:10:10 +0530

    added test.php

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Pagination-PHP
 A pagination class in PHP which supports ajax call support as well
+
+Steps to integrate :
+1) Include pagination.php file in your code
+
+2) Create a object of pagination class
+
+3) Pass the required parameters to the class as shown in test.php file
+
+4) Thats It ! Render your pagination links.
+
+Features : 
+
+	1) This pagination class also supports a call to JavaScript function as target of pagination links; in case your using ajax call for 
+	fetching the data.
+	
+	2) In that case just set render mode as 'JS' and set the JavaScript function name to be called. 
+		E.g. setRenderMode('JS')
+		Note : Don't mention brackets for function. they will be added automatically.
+	
+	3) For styling the pagination links Twitter Bootstrap classes have been used. you can customise style by editing that css sheets.		
+		
---
+++ test.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+		<?php
+
+			//include database config file (as your database configuration or query method doesnt make any differance to this pagination class,
+			// hence i have not inlcuded my database configuration file in this repository)
+			include('config.php');
+
+			//include pagination class
+			include('pagination/pagination.php');
+
+			//create an object of a pagination class
+			$pagination = new pagination();
+
+			//set number of records per page
+			$pagination->setRecordsPerPage(10);
+
+			//set links call method (PHP for simple full callback or JS for javascript function call (in case your fetching data using ajax))
+			$pagination->setRenderMode('PHP');
+			// $pagination->setRenderMode('JS');
+
+			//set javascript function name (function which you call for fetching data using ajax (if you dont have used any function, 
+			//it is mendatory to use one for using this class))
+			// $pagination->setJSCallFunction('loadData');
+
+			//get page number if its been ser in URL 
+			if(isset($_GET['page']))
+				$page = $_GET['page'];
+			else
+				$page = 1;
+
+			//set page number
+			$pagination->setPage($page);
+
+			//provide pagination class a total number of records(use count(*) query)
+			$stmt = $db->prepare('select count(*) from songs');
+			$res = $stmt->execute();
+			$total_rows = $stmt->fetch(PDO::FETCH_ASSOC);
+
+			//set number of total records
+			$pagination->setTotalRecords($total_rows['count(*)']);
+
+			//calculate offset (copy and paste this to your code)
+			$offset = ($page - 1) * $pagination->getRecordsPerPage();
+
+			//get actual data (use any type of SQL query you want and just append offset parameter to it as shown below)
+			$stmt = $db->prepare('SELECT * FROM songs LIMIT '.$offset.','.$pagination->getRecordsPerPage());
+
+			$res = $stmt->execute();
+
+			$arrSongs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+			echo '<h1>Songs</h1>';
+			echo '<table class="table table-hover">';
+
+			foreach ($arrSongs as $key => $value) {
+				echo '<tr><td>'.$value['song'].'</td></tr>';
+			}
+
+			echo '</table>';
+
+		?>
+		<?php echo $pagination->render(); ?>
+		</div>
+	</div>
+</div>
+</body>
+</html>			
